4842f75dcda318673fa005b3cd42a9f15c654a1e
aidant <aidan.twomey@7digital.com>
2017-01-12 16:01:57 +0000

    refactor for clarity

--- Approximation/RBFApproximation.Library/HaltonSeq.fs
+++ Approximation/RBFApproximation.Library/HaltonSeq.fs
@@ -16,27 +16,26 @@ module Geometry =
 module Sequences =
 
     let halton n dimension =
-        let primes = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31;]
-                
-        let rec createPoint i f (b:int) (result:double) =
-            if i > 0 
+        
+        let rec createPoint index (factor:double) basePrime result =
+            if index > 0 
               then 
-                let updatef = (double)f / (double)b
-                let updateResult = result + updatef * (double)(i % b)
-                createPoint ((int)(floor ((double)i/(double)b))) updatef b updateResult
+                let updatef = factor / basePrime
+                createPoint ((int)(floor ((double)index/basePrime))) updatef basePrime (result + updatef * (double)(index % (int)basePrime))
               else
                 result
-        let  enumerated = 1 |> List.unfold (fun i -> if (i > n) then None else Some(i, i+1))           
 
-        let create1D points = 
-            points |> List.map ( fun i -> Geometry.Point1D(createPoint i 1.0 2 0.0))     
+        let  naturalNumbers = 1 |> List.unfold (fun i -> if (i > n) then None else Some(i, i+1))           
 
-        let create2D points =
-            points |> List.map ( fun i -> Geometry.Point2D((createPoint i 1.0 2 0.0), createPoint i 1.0 3 0.0))
+        let create1D = 
+            naturalNumbers |> List.map ( fun i -> Geometry.Point1D(createPoint i 1.0 2.0 0.0))     
+
+        let create2D =
+            naturalNumbers |> List.map ( fun i -> Geometry.Point2D((createPoint i 1.0 2.0 0.0), createPoint i 1.0 3.0 0.0))
 
         match dimension with
-        | 1 -> create1D enumerated
-        | 2 -> create2D enumerated
+        | 1 -> create1D
+        | 2 -> create2D
         | _ -> []
             
 
